ee374a14fad166dd996633ace8f125245c6506f6
ghoshniki05 <nikighosh05@gmail.com>
2024-12-07 13:40:27 +0000

    conversion has done

---
+++ Basic/03_Conversion.js
@@ -0,0 +1,18 @@
+let age=34
+let a="23abc"
+let stringName= String(age)
+let b=Number(a)
+console.log(b);
+console.log(typeof b);
+
+
+//console.log(age)
+//console.log(stringName)
+//console.log(typeof stringName)
+
+// Nan== not a number
+//34==34
+//true=1, falese=0
+//""== false
+//"abshdj"==true
+
