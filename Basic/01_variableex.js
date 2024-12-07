7211999f1cb378b648257e99d152b73656f7bcda
ghoshniki05 <nikighosh05@gmail.com>
2024-12-07 09:21:51 +0000

    Config has done as of now

---
+++ Basic/01_variableex.js
@@ -0,0 +1,11 @@
+const acId="12345670"
+let accName="Apple"
+var accPw="343434"
+accCity="Siliguri"
+let accState;
+
+//acId="2121"-- we can not change the const value
+console.log(acId)
+console.table([acId, accName, accPw,accCity, accState])
+/* prefer not to use var in js
+beacuse of issue in  block scope  nd functional scope */
---
+++ Basic/datatype.js
@@ -0,0 +1,24 @@
+" use strict"  //to knoe js that we are using updated version
+let age=18
+let name="Niki"
+let iString=true
+
+console.log(3+
+
+    3);// not a readable code, don't write code this way
+/*primitive data type
+//int 2 to power 53
+//bigint
+//String=""
+//Boolean=true/false
+//null=standalove value(null is a object also)
+//undefined
+//symbol= to find uniqueness
+
+
+//object*/
+
+console.log(typeof age )
+console.log(typeof null);
+console.log(typeof undefined);
+
