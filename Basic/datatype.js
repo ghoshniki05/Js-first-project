e88fdbfed5f656718fec035720b91a3840dcdd1e
ghoshniki05 <nikighosh05@gmail.com>
2024-12-08 10:15:58 +0000

    dataype has done

--- Basic/datatype.js
+++ Basic/datatype.js
@@ -1,24 +1,49 @@
-" use strict"  //to knoe js that we are using updated version
-let age=18
-let name="Niki"
-let iString=true
+// " use strict"  //to know js that we are using updated version
+// let age=18
+// let name="Niki"
+// let iString=true
 
-console.log(3+
+// console.log(3+
 
-    3);// not a readable code, don't write code this way
-/*primitive data type
-//int 2 to power 53
-//bigint
-//String=""
-//Boolean=true/false
-//null=standalove value(null is a object also)
-//undefined
-//symbol= to find uniqueness
+//     3);// not a readable code, don't write code this way
+// /*primitive data type
+// //int 2 to power 53
+// //bigint
+// //String=""
+// //Boolean=true/false
+// //null=standalove value(null is a object also)
+// //undefined
+// //symbol= to find uniqueness
 
 
-//object*/
+// //object*/
 
-console.log(typeof age )
-console.log(typeof null);
-console.log(typeof undefined);
+// console.log(typeof age )
+// console.log(typeof null);
+// console.log(typeof undefined);
 
+/*primitive data type: total 7 type of primitive data types are: Number,Boolean, 
+String, Symbol,null,undefined,BigInt
+
+//refrence (Non primitive)
+Array, Object, Function*/
+
+let num=78589087467892n
+let nId=Symbol('123');
+let mId=Symbol('123')
+console.log(nId==mId);
+
+
+// let subject=["match","hindi","english"]// Array
+// console.log(typeof subject);
+
+let details={
+    name:"niki",
+    age:20
+}
+// console.log(details);
+// console.log(typeof details);
+
+const myFun=function(){
+    console.log("hello");
+}
